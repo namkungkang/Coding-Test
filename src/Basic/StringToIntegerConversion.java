1b34b442ced8b53d1b3f197ca48a1196d4ae7e08
namkungkang <namkung0131@naver.com>
2026-09-04 16:56:08 +0900

    StringToIntegerConversion

---
+++ src/Basic/StringToIntegerConversion.java
@@ -0,0 +1,11 @@
+package Basic;
+
+class StringToIntegerConversion {
+    public int StringToIntegerConversion(String n_str) {
+
+        int str = Integer.parseInt(n_str);
+
+
+        return str;
+    }
+}
