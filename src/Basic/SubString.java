78f741970a903d214be020613966b9f50fe1c293
namkungkang <namkung0131@naver.com>
2026-09-07 09:51:05 +0900

    Substring

---
+++ src/Basic/SubString.java
@@ -0,0 +1,14 @@
+package Basic;
+
+class SubString {
+    public int SubString(String my_string, String target) {
+        int answer = 0;
+      if (my_string.contains(target)) {
+            return 1;
+        }
+      else System.out.println(answer);
+
+
+        return answer;
+      }
+    }
