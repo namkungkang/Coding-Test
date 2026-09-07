40ed54bd3ce13adef90e5bd9a7d3c30c4e32cb83
namkungkang <namkung0131@naver.com>
2026-09-07 09:56:54 +0900

    Upper

---
+++ src/Basic/Upper.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import java.util.Locale;
+
+class Upper {
+    public String Upper(String myString) {
+        String answer = "";
+        return myString.toUpperCase();
+
+
+    }
+}
