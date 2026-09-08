977fef946b54098315a2901266ca68d5b6a4b1d7
namkungkang <namkung0131@naver.com>
2026-09-08 16:11:05 +0900

    CountUp

---
+++ src/Basic/CountUp.java
@@ -0,0 +1,17 @@
+package Basic;
+
+class CountUp {
+    public int[] CountUp(int start_num, int end_num) {
+        int size = end_num - start_num + 1;
+        int[] answer = new int[size];
+
+
+        for(int i = 0; i<size; i++ ) {
+            answer[i] = start_num + i;
+        }
+
+
+
+        return answer;
+    }
+}
--- src/Basic/dd.java
+++ src/Basic/dd.java
@@ -1,30 +1,6 @@
 package Basic;
 
+import java.util.Arrays;
+
 public class dd {
     public static void main(String[] args) {
-        int answer = 0;
-
-        String control = "wsdawsdassw";
-
-        for (int i =0; i <control.length() ; i++) {
-            switch (control.charAt(i)) {
-                case 'w':
-                    answer ++;
-                    break;
-                case 's':
-                    answer --;
-                    break;
-                case 'd':
-                    answer +=10;
-                    break;
-                case 'a':
-                    answer -=10;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        System.out.println(answer);  // 결과 출력!
-    }
-}
