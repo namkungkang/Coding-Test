2a026448eafc97874b962a865e28181c50607961
namkungkang <namkung0131@naver.com>
2026-09-09 13:40:22 +0900

    Flag

---
+++ src/Basic/Flag.java
@@ -0,0 +1,11 @@
+package Basic;
+
+class Flag {
+    public int Flag(int a, int b, boolean flag) {
+
+        if (flag == true) {
+            return a+b;
+        } else
+        return a-b;
+    }
+}
