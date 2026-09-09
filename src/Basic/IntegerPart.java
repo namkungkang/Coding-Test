b824bd6dc24b412ea3b1d2dd618f0080910fc4f7
namkungkang <namkung0131@naver.com>
2026-09-09 15:00:28 +0900

    IntegerPart

---
+++ src/Basic/IntegerPart.java
@@ -0,0 +1,12 @@
+package Basic;
+
+
+class IntegerPart {
+    public int IntegerPart(double flo) {
+        int answer = 0;
+
+        answer = Integer.valueOf((int) flo);
+
+        return answer;
+    }
+}
