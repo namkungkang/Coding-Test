346398f3c390938e3f3119912f6aab1da83586e7
namkungkang <namkung0131@naver.com>
2026-09-10 10:51:12 +0900

    StringRotation

---
+++ src/Basic/StringRotation.java
@@ -0,0 +1,14 @@
+package Basic;
+import java.util.Scanner;
+
+
+public class StringRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i =0 ; i<a.length(); i++) {
+            System.out.println(a.charAt(i));
+
+        }
+    }
+}
