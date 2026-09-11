355957a4f7113f0d38c4abfdb168b196f506a0e8
namkungkang <namkung0131@naver.com>
2026-09-11 10:52:42 +0900

    LastTwoElements

--- src/Basic/dd.java
+++ src/Basic/dd.java
@@ -2,22 +2,24 @@ package Basic;
 
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class dd {
     public static void main(String[] args) {
-        List<Integer> answer = new ArrayList<>();
-        int[] num_list = {2, 1, 6};
+        List<Integer> list = new ArrayList<>();
+        int [] answer = {2,7,5,1};
 
-        for (int i = 0; i < num_list.length; i++) {
-            answer.add(num_list[i]);
+        for (int i = 0; i <answer.length ; i++) {
+            list.add(answer[i]);
         }
-        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-            answer.add(num_list[num_list.length - 1] - num_list[num_list[num_list.length - 2]]);
-        } else answer.add(num_list[num_list.length - 1] * 2);
+        if (answer[answer.length-1] > answer[answer.length-2]) {
+            list.add(answer[answer.length-1 - answer[answer.length-2]]);
+        }
+        else  list.add(answer[answer.length-1] * 2);
 
-        System.out.println(answer);
+        System.out.print(list);
+
+
     }
 
 }
