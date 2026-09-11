e2fd393b21d9a876929107f99589f749b41490e7
namkungkang <namkung0131@naver.com>
2026-09-11 11:02:22 +0900

    StringConcatenation

---
+++ src/Basic/StringConcatenation.java
@@ -0,0 +1,14 @@
+package Basic;
+import java.util.Scanner;
+
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+            System.out.print(a.concat(b));
+
+    }
+}
