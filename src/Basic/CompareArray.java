48d213a6002c6090bcf1866011ebe763837b8f7f
namkungkang <namkung0131@naver.com>
2026-09-14 13:47:35 +0900

    CompareArray

---
+++ src/Basic/CompareArray.java
@@ -0,0 +1,38 @@
+package Basic;
+
+
+
+class CompareArray {
+    public int CompareArray(int[] arr1, int[] arr2) {
+        int answer =0;
+        int sum1 = 0;
+        int sum2 = 0;
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            sum1 += arr1[i];
+
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            sum2 += arr2[i];
+        }
+
+        if (arr1.length < arr2.length) {
+            return -1;
+        } else if (arr1.length > arr2.length) {
+            return 1;
+        } else if (arr1.length == arr2.length) {
+            if (sum1 > sum2) {
+                return 1;
+            } else if (sum1 < sum2)
+                return -1;
+        }
+            else if (sum1 == sum2){
+                return -1;
+        } else  return 0;
+
+
+
+        return answer;
+    }
+}
