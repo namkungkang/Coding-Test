5649b468b0fc692faa242e211e4be3c4875ea0e4
namkungkang <namkung0131@naver.com>
2026-09-14 14:43:44 +0900

    SwitchString

---
+++ src/Basic/SwitchString.java
@@ -0,0 +1,18 @@
+package Basic;
+
+class SwitchString {
+    public int SwitchString(String myString, String pat) {
+        String answer = "";
+        for(int i= 0; i < myString.length(); i++) {
+            if(String.valueOf(myString.charAt(i)).equals("A")) {
+                answer += "B";
+            }
+            else answer +="A";
+            if(answer.contains(pat)) {
+                return 1;
+            }
+
+        }
+         return 0;
+    }
+}
--- src/Basic/dd.java
+++ src/Basic/dd.java
@@ -1,23 +1,25 @@
 package Basic;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class dd {
     public static void main(String[] args) {
-        int [] arr ={49, 12, 100, 276, 33};
-        int  n = 27;
-        if(arr.length % 2 != 0){
-            for(int i = 0; i < arr.length; i += 2){
-                arr[i] += n;
-            }
-        }
-        else{
-            for(int i = 1; i < arr.length; i += 2){
-                arr[i] += n;
-            }
-        }
-        System.out.println(Arrays.toString(arr));
+    String myString = "ABBAA";
+    String pat = "AABB";
+    String answer = "";
 
+        for (int i = 0; i <myString.length() ; i++) {
+            if (myString.charAt(i) == 'A') {
+                answer+="B";
+            }
+            else answer+="A";
+
+        }
+       if (answer.contains(pat)) {
+           System.out.println(1);
+       }
+       else System.out.println(0);
     }
 }
