d08f8d51bdd10cf6b38f8fabd54ab6402de7c00f
namkungkang <namkung0131@naver.com>
2026-09-15 10:43:38 +0900

    SpaceeSeparator2

---
+++ src/Basic/SpaceeSeparator2.java
@@ -0,0 +1,12 @@
+package Basic;
+
+
+class SpaceeSeparator2 {
+    public String[] SpaceeSeparator2(String my_string) {
+        String[] answer = my_string.split(" ");
+
+
+
+        return answer;
+    }
+}
