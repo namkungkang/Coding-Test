2ba46e26c8551a02efb2e730f847864807b0c0e4
namkungkang <namkung0131@naver.com>
2026-09-15 14:21:54 +0900

    TransformSequence

--- src/Basic/TransformSequence.java
+++ src/Basic/TransformSequence.java
@@ -3,14 +3,8 @@ package Basic;
 
 import java.util.Arrays;
 
-public class dd {
-    public static void main(String[] args) {
-
-        int[] arr = {1, 2, 3, 100, 99, 98} ;
-        int size = arr.length;
-        int[] answer = new int[size];
-        int index =0;
-
+class TransformSequence {
+    public int[] TransformSequence(int[] arr) {
         for (int i = 0; i <arr.length ; i++) {
             if (arr[i] > 50 && arr[i] % 2 == 0) {
                 arr[i] = arr[i] / 2;
@@ -19,8 +13,7 @@ public class dd {
             }
         }
 
-
-        System.out.println(Arrays.toString(arr));
+        return Arrays.stream(arr).toArray();
 
     }
 }
