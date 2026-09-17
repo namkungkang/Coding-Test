0276e29a18b408d2dc94dc4891277ed389d728a1
namkungkang <namkung0131@naver.com>
2026-09-17 13:33:47 +0900

    ManipulatingNumbers1

--- src/Basic/dd.java
+++ src/Basic/dd.java
@@ -7,15 +7,31 @@ import java.util.Arrays;
 
 public class dd {
     public static void main(String[] args) {
+        String answer = "";
+        int [] numLog = {0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1};
+        for (int i = 1; i <numLog.length ; i++) {
+            switch (numLog[i] - numLog[i-1]){
+                case 1 :
+                    answer+='w';
+                    break;
+                case  -1:
+                    answer+='s';
+                    break;
+                case +10:
+                    answer+='d';
+                    break;
+                case -10:
+                    answer+='a';
+                    break;
+                default:
+                    break;
+            }
 
-        ArrayList<Integer> list = new ArrayList<>();
-        int n = 10;
-        list.add(n);
-        while (n>1) {
-            n = n % 2 ==0 ? n/2 : n*3 + 1;
-            list.add(n);
+
         }
+        System.out.println(answer);
+
+    }
 
 
-        System.out.println(list);
-    }}
+}
