d5b00455e0e2affb0c13b8eb5993f184664d8f94
namkungkang <namkung0131@naver.com>
2026-09-17 13:57:52 +0900

    PrintAddition

---
+++ src/Basic/PrintAddition.java
@@ -0,0 +1,14 @@
+package Basic;
+import java.util.Scanner;
+
+public class PrintAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a+b;
+
+        System.out.printf("%d + %d = %d",a,b,result);
+    }
+
+}
