e6b7541520f1a07a2abaa02ed599f8703d79ffae
namkungkang <namkung0131@naver.com>
2026-09-18 10:01:15 +0900

    MakeOne

--- src/Basic/dd.java
+++ src/Basic/dd.java
@@ -6,11 +6,18 @@ import java.util.Arrays;
 public class dd {
     public static void main(String[] args) {
         int answer = 0;
-        int [] date1 = {2021,12,28};
-        int [] date2 = {2021,12,29};
+        int [] num_list = {12,4,15,1,14};
+        for (int i = 0; i <num_list.length ; i++) {
+          while (num_list[i] != 1) {
+              if (num_list[i] % 2 == 0) {
+                  num_list[i] = num_list[i] /2;
+              } else num_list[i] = num_list[i] -1 /2;
+            answer++;
+          }
+        }
 
 
 
-        System.out.println(Arrays.toString(date1));
+
     }
 }
